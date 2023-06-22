3b08b4c65f8242ac5c04da2fa9187b01ae4d288e
Eunseo-jo <90231153+Eunseo-jo@users.noreply.github.com>
2023-06-23 02:30:42 +0900

    Day5 - hashtable & graph

--- Day5/[실습] 베스트앨범.js
+++ Day5/[실습] 베스트앨범.js
@@ -1,61 +1,44 @@
 function solution(genres, plays) {
-    var answer = [];
-    /*
-        속한 노래가 많이 재생된 장르를 먼저 수록합니다.
-        장르 내에서 많이 재생된 노래를 먼저 수록합니다.
-        장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록합니다.
-    */
-    const table = new Map();
     const map = new Map();
-    for(let i = 0; i < genres.length; i++){
-        
-        if(!map.has(genres[i])) {
-            map.set(genres[i], plays[i]);
-            table.set(genres[i], [[i, plays[i]]]);
-        }
-        else{
-            table.set(genres[i],  ...table.get(genres[i]) , [ i, plays[i]]);
-            map.set(genres[i], map.get(genres[i])+plays[i]);
+    for (let i = 0; i < genres.length; i++) {
+        if (!map.has(genres[i])) {
+            //장르가 map에 없을 때
+            map.set(genres[i], {
+                total: plays[i],
+                songs: [{ play: plays[i], index: i }],
+            });
+        } else {
+            //이미 장르가 map안에 있을 떄
+            map.set(genres[i], {
+                total: map.get(genres[i]).total + plays[i],
+                songs: [...map.get(genres[i]).songs, { play: plays[i], index: i }]
+                    .sort((a, b) => b.play - a.play)
+                    .slice(0, 2),
+            });
         }
     }
-
-    let mapToArray = [...map];
-    let tableToArray = [...table.values()];
-    const sortedMap = new Map(mapToArray.sort((a, b) => b[1] - a[1]));
-    console.log(tableToArray);
-    const sortedTable = new Map(tableToArray.sort((a, b) => {
-        if(a[1][0] === b[1][0]) {
-            if(a[1][1] === b[1][1]) return a[0]-b[0];
-            else return b[1][1] - a[1][1];
-        }
-        else return b[1][0] - a[1][0];
-        
-    }));
-    console.log(sortedMap);
-    console.log(table);
-    console.log(sortedTable);
-    return answer;
+    return [...map.entries()]
+        .sort((a, b) => b[1].total - a[1].total)
+        .flatMap((item) => item[1].songs)
+        .map((song) => song.index);
 }
-
-console.log(solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500])); //[4,1,3,0]
-
-function solution(genres, plays){
+//모범 답안 코드
+function solution(genres, plays) {
     const map = new Map();
 
-    genres.map((genre, index) => [genre, plays[index]])
-    .forEach(([genre, play], index) => {
-        const data = map.get(genre) || {total: 0, songs: [] };
-        map.set(genre, {
-            total: data.total + play,
-            songs: [...data.songs, {play, index}]
-            .sort((a,b) => b.play - a.play)
-            .slice(0,2)
-        })
-        
-        
-    });
-    return([...map.entries()]
-        .sort((a,b) => b[1].total - a[1].total)
-        .flatMap(item => item[1].songs)
-        .map(song => song.index))
+    genres
+        .map((genre, index) => [genre, plays[index]])
+        .forEach(([genre, play], index) => {
+            const data = map.get(genre) || { total: 0, songs: [] };
+            map.set(genre, {
+                total: data.total + play,
+                songs: [...data.songs, { play, index }].sort((a, b) => b.play - a.play).slice(0, 2),
+            });
+        });
+    return [...map.entries()]
+        .sort((a, b) => b[1].total - a[1].total)
+        .flatMap((item) => item[1].songs)
+        .map((song) => song.index);
 }
+
+console.log(solution(['classic', 'pop', 'classic', 'classic', 'pop'], [500, 600, 150, 800, 2500])); //[4,1,3,0]
--- Day5/[실습] 프로세스.js
+++ Day5/[실습] 프로세스.js
@@ -1,22 +1,21 @@
 class Node {
-    constructor(value){
-        this.value = value; //값
-        this.next = null; //포인터
+    constructor(value) {
+        this.value = value;
+        this.next = null;
     }
 }
 
-class Queue { //노드끼리 엮어주는 역할
-    constructor(){
+class Queue {
+    constructor() {
         this.head = null;
         this.tail = null;
         this.length = 0;
     }
-    enqueue(newValue){
+    enqueue(newValue) {
         const newNode = new Node(newValue);
-        if(this.head === null){ //아무 값이 없을 때
+        if (this.head === null) {
             this.head = this.tail = newNode;
-        }
-        else{ //값 존재
+        } else {
             this.tail.next = newNode;
             this.tail = newNode;
         }
@@ -39,24 +38,32 @@ class Queue { //노드끼리 엮어주는 역할
 }
 
 function solution(priorities, location) {
-    let queue = new Queue;
-    for(let i = 0; i < priorities.length; i++){
+    let queue = new Queue();
+    for (let i = 0; i < priorities.length; i++) {
         queue.enqueue([priorities[i], i]);
     }
-    priorities.sort((a,b) => b-a);
+    priorities.sort((a, b) => b - a);
 
     let cnt = 0;
-    while(queue.size() > 0){
+    while (queue.size() > 0) {
         const currValue = queue.peek();
-        if(currValue[0] < priorities[cnt]){
-            queue.enqueue(queue.dequeue())
-        }
-        else{
-            const value = queue.dequeue();    
+        if (currValue[0] < priorities[cnt]) {
+            queue.enqueue(queue.dequeue());
+        } else {
+            const value = queue.dequeue();
             cnt += 1;
-            if(value[1] === location) return cnt;
+            if (value[1] === location) return cnt;
         }
     }
-    
+    /*
+    while (queue.size()) {
+        const currValue = queue.dequeue();
+        if (currValue[0] < priorities[cnt]) {
+            queue.enqueue(currValue);
+        } else {
+            cnt++;
+            if (currValue[1] === location) return cnt;
+        }
+    }
+*/
 }
-
